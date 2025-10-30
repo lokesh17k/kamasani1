9cd4f473f0c957d888ff897cb7ae5540390a8b3d
lokesh17k <commvaultbatch3@gmail.com>
2025-10-30 14:05:26 +0530

    Update rooms.js
    removed developer details and author details

--- frontend/data/rooms.js
+++ frontend/data/rooms.js
@@ -1,8 +1,8 @@
 /**
  * @name Hotel Room Booking System
- * @author Md. Samiur Rahman (Mukul)
- * @description Hotel Room Booking and Management System Software ~ Developed By Md. Samiur Rahman (Mukul)
- * @copyright ©2023 ― Md. Samiur Rahman (Mukul). All rights reserved.
+ * @author Md.
+ * @description Hotel Room Booking and Management System Software ~ 
+ * @copyright ©2023 
  * @version v0.0.1
  *
  */
