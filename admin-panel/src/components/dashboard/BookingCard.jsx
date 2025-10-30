a9ad99f0a4b32844df1db5f4bcde875ceee12921
lokesh17k <commvaultbatch3@gmail.com>
2025-10-30 11:59:12 +0530

    Update BookingCard.jsx

--- admin-panel/src/components/dashboard/BookingCard.jsx
+++ admin-panel/src/components/dashboard/BookingCard.jsx
@@ -1,8 +1,5 @@
 /**
- * @name Hotel Room Booking System
- * @description Hotel Room Booking and Management System Software 
- * @copyright ©2023 ― All rights reserved.
- * @version v0.0.1
+
  *
  */
 
